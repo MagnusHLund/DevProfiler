a6ac550eeea6ba3dfb5eda5ee294ce665251c915
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-11-29 22:39:34 +0100

    Created output formatter

--- utils/devprofiler-formatter.php
+++ utils/devprofiler-formatter.php
@@ -4,3 +4,20 @@
 if (! defined('ABSPATH')) {
     exit;
 }
+
+class DevProfiler_Formatter
+{
+
+    /**
+     * @param array $parameters Associative array.
+     */
+    public static function format_output($parameters)
+    {
+        $formatted_output = "";
+        $formatted_output .= array_map(function ($key, $value) {
+            return "$key: ($value)";
+        }, $parameters);
+
+        return $formatted_output;
+    }
+}
